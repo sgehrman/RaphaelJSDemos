7b71248e988a1f2d0ea7fa06115a8ed2313c733e
Steve Gehrman <sgehrman@gmail.com>
2012-10-23 01:54:13 -0700

    fix

---
+++ animationSetup.js
@@ -0,0 +1,31 @@
+  function setupAnimations() {
+    var theGradient = "90-#aaf-#004";
+    var paper = Raphael(0, 280, 650, 650);
+
+    paper.rect(0, 0, 850, 650).attr({
+      fill: theGradient,
+      stroke: '#f99',
+      title: 'background'
+    });
+
+    var animations = [
+    new PathAnimation('#44f', 0), new PathAnimation('#f33', 100), new PathAnimation('#2f2', 200), new PathAnimation('#232', 300), new PathAnimation('#f1f', 400)];
+
+    animations.forEach(function(el) {
+      el.setup(paper);
+    });
+
+
+    var controlPaper = Raphael(0, 100, 50, 50);
+
+    var circle = controlPaper.circle(12, 12, 12).attr({
+      fill: '#333'
+    });
+
+    circle.node.onclick = function() {
+      // tell all the animations to run
+      animations.forEach(function(el) {
+        el.animate();
+      });
+    }
+  }
