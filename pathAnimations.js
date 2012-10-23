468e3a26fcf3ee1bc9a87fe575519b9ecd220791
Steve Gehrman <sgehrman@gmail.com>
2012-10-22 23:22:18 -0700

    fix

--- pathAnimations.js
+++ pathAnimations.js
@@ -1,45 +1,50 @@
+function PathAnimation(inColor) {
+	this.fillColor = inColor;
 
-function setupAnimation() {
-	var paper = Raphael(0, 280, 650, 650);
+	// method
+	this.setup = setup;
 
-	var theGradient = "90-#aaf-#004";
+	function setup() {
+		var paper = Raphael(0, 280, 650, 650);
 
-	paper.rect(0, 0, 650, 650).attr({
-		fill: theGradient,
-		'fill-opacity': .4,
-		stroke: '#f99',
-		title: 'background'
-	});
+		var theGradient = "90-#aaf-#004";
 
-	var rectPath = makeRectanglePath(0, 0, 100, 400, 8, 8, 8, 8);
-	var pathSwitch = false;
-
-	var theRect = paper.path(rectPath).attr({
-		fill: '#333'
-	});
-
-	var theGear = gear[4].path;
-	theGear = normalizePath(theGear);
-	theGear = scalePath(theGear, .5);
-
-	theRect.attr({
-		title: 'far out man'
-	});
-
-	function animateNext() {
-		var thePath = rectPath;
-		if(+(pathSwitch = !pathSwitch)) thePath = theGear;
-
-		theRect.animate({
-			path: thePath,
-			fill: Raphael.getColor(),
-			'fill-opacity': 0.4
-		}, 400, "bounce", function() {
-			console.log("animation done");
+		paper.rect(0, 0, 850, 650).attr({
+			fill: theGradient,
+			stroke: '#f99',
+			title: 'background'
 		});
-	}
 
-	theRect.node.onclick = function() {
-		animateNext();
+		var rectPath = makeRectanglePath(0, 0, 100, 400, 8, 8, 8, 8);
+		var pathSwitch = false;
+
+		var theRect = paper.path(rectPath).attr({
+			fill: this.fillColor
+		});
+
+		var theGear = gear[4].path;
+		theGear = normalizePath(theGear);
+		theGear = scalePath(theGear, .5);
+
+		theRect.attr({
+			title: 'far out man'
+		});
+
+		var that = this;
+
+		function animateNext() {
+			var thePath = thePath = (+(pathSwitch = !pathSwitch)) ? theGear : rectPath;
+
+			theRect.animate({
+				path: thePath,
+				fill: that.fillColor,
+			}, 400, "bounce", function() {
+				console.log("animation done");
+			});
+		}
+
+		theRect.node.onclick = function() {
+			animateNext();
+		}
 	}
 }
