1ed517115adfdc4c7e13c67335154d2aaadbd208
Steve Gehrman <sgehrman@gmail.com>
2012-11-08 04:16:40 -0800

    fix

--- demo3/animationSetup.js
+++ demo3/animationSetup.js
@@ -41,3 +41,114 @@
       });
     }
   }
+
+function setupPathFields()
+{
+    var $ = jQuery;
+
+   var oneTextArea = $('#one');
+   var twoTextArea = $('#two');
+
+   oneTextArea.text(gearPath);
+   twoTextArea.text(circleGearPath);
+
+    $("#cunt").on("click", function(event){
+        
+     console.log(oneTextArea.text());
+     console.log(twoTextArea.text());
+
+    });
+
+
+  }
+
+function removeGearHole(theGear) {
+    var theArray = Raphael.parsePathString(theGear);
+    var found = -1;
+    var index = 0;
+
+    var theResult = theArray.some(function(item) {
+      index += 1;
+
+      if(item.toString() === "z") {
+        found = index;
+        return true;
+      }
+
+      return false;
+    });
+
+    if(found !== -1) 
+      return theArray.slice(0, found);
+  }
+
+function pathOne(offset)
+{
+  // private vars
+  // getting the gears out of "cog.js"
+  var gear = gearPath;
+
+  // modify paths
+  gear = normalizePath(gear);
+  gear = scalePath(gear, .5);
+  gear = translatePath(gear, offset, 0);
+  // gear = rotatePath(gear, 180);
+
+  // removes the hole in the cog
+    if(false) {
+      gear = removeGearHole(gear);
+    }
+
+    return gear;
+}
+
+function pathTwo(offset) {
+   var circleGear = circleGearPath;
+ 
+  circleGear = normalizePath(circleGear);
+  circleGear = scalePath(circleGear, .5);
+  circleGear = translatePath(circleGear, offset, 0);
+  circleGear = rotatePath(circleGear, 180);
+
+return circleGear;
+}
+
+function PathAnimation(inColor, inOffset) {
+  this.fillColor = inColor;
+  this.offset = inOffset;
+
+  var pathSwitch = true;
+
+  // method
+  this.setup = setup;
+  this.animate = animate;
+
+  // this is dumb
+  var that = this;
+
+  function setup(paper) {
+      // not used now, saving 
+    // this.startPath = makeRectanglePath(0, 0, 100, 500);
+    // this.startPath = translatePath(this.startPath, this.offset, 0);
+  
+    this.mainPath = paper.path(pathOne(this.offset)).attr({
+      fill: this.fillColor
+    });
+
+    this.mainPath.node.onclick = function() {
+      that.animate();
+      that.mainPath.toFront();
+    }
+  }
+
+  function animate() {
+    var thePath  = (+(pathSwitch = !pathSwitch)) ? pathOne(this.offset) : pathTwo(this.offset);
+
+    this.mainPath.animate({
+      path: thePath,
+      fill: this.fillColor,
+    }, 800, "<>");
+  }
+
+}
+
